6157a368893c6888fa4fad6cfc5a4a2df53fb45f
ranjan-dev40 <roy984258@gmail.com>
2023-07-10 07:08:04 +0530

    modify app.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import { ChatEngine } from "react-chat-engine";
+import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed';
+import Modal from './components/LoginForm';
 import './App.css';
-import ChatFeed from './components/ChatFeed.jsx'
 
-// import LoginForm from './components/LoginForm'
-import Modal from "./components/LoginForm";
-
+const projectID = "a93f9594-2b9e-4ea5-aa54-1244ad734437";
 
 const App = () => {
+  if (!localStorage.getItem('username')) return <Modal />;
 
-    if(!localStorage.getItem('username')) return <Modal />
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
 
-    return(
-        <ChatEngine 
-            height="100vh"
-            projectID="a93f9594-2b9e-4ea5-aa54-1244ad734437"
-            userName={localStorage.getItem('username')}
-            userSecret={localStorage.getItem('password')}
-            renderChatFeed={(chatAppProps) => <ChatFeed  {...chatAppProps } />}
-
-        />
-    )
-}
+// infinite scroll, logout, more customizations...
 
 export default App;
-
-
